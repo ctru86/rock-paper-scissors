84c927fda0d7ada9b3963f54e7c14dd9600cecfb
ctru86 <224899882+ctru86@users.noreply.github.com>
2025-10-05 20:42:11 -0400

    Buils the game into the browser console interface.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock-paper-scissors
+
+This is my attempt to complete the first Java Script exercise on The Odin Project. 
---
+++ rps5.js
@@ -0,0 +1,79 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getHumanChoice() {
+    const selection = prompt("Try your hand at Paper Rock Scissors. Type your selection: ");
+    const humanchoice = selection.toLowerCase();
+    console.log("You chose: " + humanchoice);
+    return humanchoice;
+}
+
+
+function computerChoice() {
+    const compSelection = Math.random();
+    if (compSelection <= .33) {
+        return "rock";
+    } else if (compSelection <= .67) {
+        return "paper";
+    } else {
+        return "scissors";
+    } 
+}
+
+function playRound(humanChoice, compSelection) {
+    console.log("Your opponent chose: " + compSelection);
+
+    if (compSelection === humanChoice) {
+        console.log("Draw");
+    }    
+    else if (compSelection === "rock" && humanChoice  === "paper") {
+        console.log("You win! Paper beats rock.");
+        humanScore++;   
+    }
+    else if (compSelection === "rock" && humanChoice  === "scissors") {
+        console.log("You lose! Rock beats scissors.");
+        computerScore++;  
+    }
+    else if (compSelection === "paper" && humanChoice  === "rock") {
+        console.log ("You lose! Paper beats rock.");
+        computerScore++;  
+    }
+    else if (compSelection === "paper" && humanChoice  === "scissors") {
+        console.log ("You win! Scissors beats paper.");
+        humanScore++;
+    }
+    else if (compSelection === "scissors" && humanChoice === "paper") {
+        console.log ("You lose! Scissors beat paper.");
+        computerScore++;
+    }
+    else if (compSelection === "scissors" && humanChoice === "rock") {
+        console.log ("You win! Rock beats scissors.");
+        humanScore++;
+    }
+
+    console.log("The score is: you, " + humanScore + " vs Computer, " + computerScore);
+}
+
+
+function playGame() {
+    playRound(getHumanChoice(), computerChoice());
+    playRound(getHumanChoice(), computerChoice());
+    playRound(getHumanChoice(), computerChoice());
+    playRound(getHumanChoice(), computerChoice());
+    playRound(getHumanChoice(), computerChoice());
+
+    if (humanScore > computerScore) {
+        console.log("You win! Congrats!")
+    } else if (humanScore < computerScore) {
+        console.log("You lose! Try again.")
+    } else {
+        console.log("Tie!")
+    }
+}
+
+playGame(); 
+
+
+
+
+
